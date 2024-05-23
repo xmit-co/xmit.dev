f7103b41d640f7dcf8687b5416508bacfc9ac15e
Pierre Carrier <pc@rrier.fr>
2024-05-23 09:56:41 +0200

    add feed.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 module.exports = function (cfg) {
   cfg.addPassthroughCopy("img");
+  cfg.addFilter("rfc3339", (date) => new Date(date).toISOString());
+  cfg.addFilter("stringify", (v) => JSON.stringify(v));
   cfg.addFilter(
     "dateDisplay",
     (date) => new Date(date).toISOString().split("T")[0],
   );
+  cfg.setNunjucksEnvironmentOptions({
+    autoescape: false,
+  });
   return {
     dir: {
       output: "dist",
